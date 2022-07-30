5b77427008ed94f7b33eea9c8400ba833513144e
Julia <gordinyulia@gmail.com>
2022-07-30 18:35:38 +0300

    first task "I can win"

---
+++ src/main/java/com/epam/training/student_name_surname/iCanWin/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_name_surname.iCanWin;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello CI!" );
+    }
+
+    public static void doNothing() {
+        // do nothing
+    }
+}
